4525519e11f83f4375daa94c075f3d744a7161f4
Anderson Gomes <djandersongomesdnb@gmail.com>
2018-06-19 20:17:42 -0300

    operação de saque

--- main.py
+++ main.py
@@ -1,6 +1,33 @@
 import getpass
 import os
 
+account_list = {
+    '0001-02': {
+        'password': '12345',
+        'name': 'José',
+        'value': 100,
+        'admin': False
+    },
+    '0001-03': {
+        'password': '123456',
+        'name': 'Maria',
+        'value': 50,
+        'admin': False
+    },
+    '0001-04': {
+        'password': '123456',
+        'name': 'Anderson',
+        'value': 1000,
+        'admin': True
+    },
+}
+
+money_slips = {
+    '20': 5,
+    '50': 5,
+    '100': 5,
+}
+
 while True:
     print("*********************************************")
     print("***************Caixa Eletrônico**************")
@@ -9,34 +36,6 @@ while True:
     account_typed = input('Digite sua conta: ')
     password_typed = getpass.getpass('Digite sua senha: ')
     print(account_typed)
-
-    #Agência, Senha, Nome, Valor
-    account_list = {
-        '0001-02': {
-            'password': '12345',
-            'name': 'José',
-            'value': 1000,
-            'admin': False
-        },
-        '0001-03': {
-            'password': '123456',
-            'name': 'Maria',
-            'value': 2000,
-            'admin': False
-        },
-        '0001-04': {
-            'password': '123456',
-            'name': 'Anderson',
-            'value': 2000,
-            'admin': True
-        },
-    }
-
-    money_slips = {
-        '20': 500,
-        '50': 500,
-        '100': 500,
-    }
 
     if account_typed in account_list and password_typed == account_list[account_typed]['password']:
         clear = 'cls'if os.name == 'nt' else' clear'
@@ -50,6 +49,7 @@ while True:
             print('Seja bem Vindo! %s' % account_list[account_typed]['name'])
 
         print("1 - Saldo")
+        print("2 - saque")
         if account_list[account_typed]['admin']:
             print("10 - Incluir Cédula")
         option_typed = input('Escolha uma opção acima: ')
@@ -60,6 +60,29 @@ while True:
             money_bill_typed = input('Digite a cédula a ser incluida: ')
             money_slips[money_bill_typed] += int(amount_typed)
             print(money_slips)
+        elif option_typed == '2':
+            value_typed = input('Digite o valor a ser sacado: ')
+
+            money_slips_user = {}
+            value_int = int(value_typed)
+
+            if value_int // 100 > 0 and value_int // 100 <= money_slips['100']:
+                money_slips_user['100'] = value_int // 100
+                value_int = value_int - value_int // 100 * 100
+
+            if value_int // 50 > 0 and value_int // 50 <= money_slips['50']:
+                money_slips_user['50'] = value_int // 50
+                value_int = value_int - value_int // 50 * 50
+
+            if value_int // 20 > 0 and value_int // 20 <= money_slips['20']:
+                money_slips_user['20'] = value_int // 20
+                value_int = value_int - value_int // 20 * 20
+
+            if value_int != 0:
+                print("O caixa não tem cédula disponível para este valor")
+            else:
+                print('Pegue as notas: ')
+                print(money_slips_user)
     else:
         print('Conta Inválida')
 
